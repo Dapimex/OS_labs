2e50b7a844477d2ff8845bf7a3d8a34088937d5f
Dapimex <d.pimenov@innopolis.ru>
2018-09-12 21:26:08 +0300

    Comment

---
+++ ex3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define TRUE 1
+
+int main() {
+	char command[100];
+	while (TRUE) {
+		printf("> ");
+		scanf("%s", command);
+		system(command);
+	}
+	return 0;
+}
