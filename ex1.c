31219820e36bcb2088556e04b9e8e25245180f60
Dapimex <d.pimenov@innopolis.ru>
2018-11-08 18:45:40 +0300

    week12

---
+++ ex1.c
@@ -0,0 +1,10 @@
+//#include <stdio.h>
+//#include <stdlib.h>
+//
+//int main() {
+//    FILE *file_random = fopen("/dev/random", "r");
+//    char *random_array = (char*)malloc(sizeof(char));
+//    fread(random_array, 20, sizeof(char), file_random);
+//    printf(random_array);
+//    return 0;
+//}
